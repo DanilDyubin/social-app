2fd031977d0b278185f54c82fe332360740c3f6b
Danil Dyubin <danildyubin@MacBook-Air-Danil.local>
2024-07-01 18:02:46 +0300

    app version 1.1

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,6 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyCeo2J-ftjA-DKc4Th_2BnMQuQy3VMMAv0',
-//   authDomain: 'socialapp-e45c4.firebaseapp.com',
-//   projectId: 'socialapp-e45c4',
-//   storageBucket: 'socialapp-e45c4.appspot.com',
-//   messagingSenderId: '176241754225',
-//   appId: '1:176241754225:web:289c80bd233d74e09e5bd0',
-// };
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
