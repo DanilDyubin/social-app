a559cd6ee332d832be3ed749cc1333c9f71b6bbe
Danil Dyubin <danildyubin@MacBook-Air-Danil.local>
2024-07-01 18:01:08 +0300

    v. 1.1

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,21 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyCeo2J-ftjA-DKc4Th_2BnMQuQy3VMMAv0',
+//   authDomain: 'socialapp-e45c4.firebaseapp.com',
+//   projectId: 'socialapp-e45c4',
+//   storageBucket: 'socialapp-e45c4.appspot.com',
+//   messagingSenderId: '176241754225',
+//   appId: '1:176241754225:web:289c80bd233d74e09e5bd0',
+// };
 const firebaseConfig = {
-  apiKey: 'AIzaSyCeo2J-ftjA-DKc4Th_2BnMQuQy3VMMAv0',
-  authDomain: 'socialapp-e45c4.firebaseapp.com',
-  projectId: 'socialapp-e45c4',
-  storageBucket: 'socialapp-e45c4.appspot.com',
-  messagingSenderId: '176241754225',
-  appId: '1:176241754225:web:289c80bd233d74e09e5bd0',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STOEAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
